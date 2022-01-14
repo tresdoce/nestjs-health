c01c51032d160294083391820e79dc43ec2093cf
Mex Delgado <mdelgado@tresdoce.com.ar>
2022-01-14 17:47:42 -0300

    chore(dependencies): update dependencies to latest and stable versions

--- src/health/controllers/readiness.controller.ts
+++ src/health/controllers/readiness.controller.ts
@@ -1,25 +1,26 @@
-import {Controller, Get, Inject} from '@nestjs/common';
-import {HealthCheck, HealthCheckService, HttpHealthIndicator} from '@nestjs/terminus';
-import {CONFIG_OPTIONS} from '../constants';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+import { URL } from 'url';
+
+import { CONFIG_OPTIONS } from '../constants';
 
 @Controller()
 export class ReadinessController {
-    constructor(
-        @Inject(CONFIG_OPTIONS) private readonly appConfig: any,
-        private health: HealthCheckService,
-        private http: HttpHealthIndicator,
-    ) {
-    }
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly appConfig: any,
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
 
-    @Get('readiness')
-    @HealthCheck()
-    async check() {
-        const servicesPingCheckList = Object.keys(this.appConfig.services).map((key) => {
-            const urlService = new URL(this.appConfig.services[key]);
-            return () => this.http.pingCheck(`${key}`, `${urlService.origin}`);
-        });
+  @Get('readiness')
+  @HealthCheck()
+  async check() {
+    const servicesPingCheckList = Object.keys(this.appConfig.services).map((key) => {
+      const urlService = new URL(this.appConfig.services[key]);
+      return () => this.http.pingCheck(`${key}`, `${urlService.origin}`);
+    });
 
-        const response = this.health.check([...servicesPingCheckList]);
-        return (await response).info;
-    }
+    const response = this.health.check([...servicesPingCheckList]);
+    return (await response).info;
+  }
 }
