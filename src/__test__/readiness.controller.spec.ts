75ddb6a5dd07be445f9bfeab958baf9365300b53
Mex Delgado <mdelgado@tresdoce.com.ar>
2022-01-22 01:41:18 -0300

    chore: update dependencies

--- src/__test__/readiness.controller.spec.ts
+++ src/__test__/readiness.controller.spec.ts
@@ -5,34 +5,33 @@ import { ReadinessController } from '../health/controllers/readiness.controller'
 import { CONFIG_OPTIONS } from '../health/constants';
 
 const config = {
-    services: {
-        rickAndMortyAPI: 'https://rickandmortyapi.com/api/character/1',
-    },
+  services: {
+    rickAndMortyAPI: 'https://rickandmortyapi.com/api/character/1',
+  },
 };
 
 describe('ReadinessController', () => {
-    let controller: ReadinessController;
+  let controller: ReadinessController;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [TerminusModule, HttpModule],
+      controllers: [ReadinessController],
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: config,
+        },
+      ],
+    }).compile();
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            imports: [TerminusModule, HttpModule],
-            controllers: [ReadinessController],
-            providers: [
-                {
-                    provide: CONFIG_OPTIONS,
-                    useValue: config,
-                },
-            ],
-        }).compile();
+    controller = module.get<ReadinessController>(ReadinessController);
+  });
 
-        controller = module.get<ReadinessController>(ReadinessController);
-    });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
-    });
-
-    it('should be return up services', async () => {
-        expect(await controller.check()).toEqual({ rickAndMortyAPI: { status: 'up' } });
-    });
+  it('should be return up services', async () => {
+    expect(await controller.check()).toEqual({ rickAndMortyAPI: { status: 'up' } });
+  });
 });
